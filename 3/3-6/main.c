f1f27346e4a0bdc0011cf2cdd8d5aa2f7219dd53
Kkvvnn <vova6281527@gmail.com>
2016-12-29 00:15:22 +0500

    3.6 Ready

---
+++ 3/3-6/main.c
@@ -0,0 +1,22 @@
+/*		Упражнение 3.6                                                                           *
+*	Напишите версию itoa с дополнительным третьим аргументом, задающим минимальную ширину поля.  *
+*	При необходимости преобразованное число должно слева дополняться пробелами.                  */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 100
+
+extern void itoa(int n, char s[], int width);
+
+int main(void)
+{
+    int pattern = -1234567;
+	int width = 15;
+    char line_number[MAXLINE];
+
+    itoa(pattern, line_number, width);
+    printf("%s\t%d\n", line_number, strlen(line_number));
+
+    return 0;
+}
