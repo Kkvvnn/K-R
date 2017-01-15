c5af8435875c10d7637a3e7cbc6b6b013e29aabc
Kkvvnn <vova6281527@gmail.com>
2017-01-16 02:20:25 +0500

    4.3 добавил поддержку отрицательных операндов

--- 4/4-3/getop.c
+++ 4/4-3/getop.c
@@ -11,20 +11,31 @@ extern void ungetch(int);
 int getop(char s[])
 {
     int i, c;
+    int c_next;
 
+    /* пропускаем табуляцию и пробелы */
     while ((s[0] = c = getch()) == ' ' || c == '\t');
 
     s[1] = '\0';
 
-    if (!isdigit(c) && c != '.')
-        return c;                /* не число */
-    
+    if (!isdigit(c) && c != '.' && c != '-')
+        return c;                               /* не число и не знак минус '-'*/
+
+    if (c == '-')                               /* если после минуса... */
+        if (isspace(c_next = getch()))          /* ...следующий символ - символ пустого пространства */
+        {
+            ungetch(c_next);                    /* возвращаем его на ввод */
+            return c;                           /* оператор '-' возвращаем в main() */
+        }
+        else 
+            ungetch(c_next);                   
+
     i = 0;
     
-    if (isdigit (c))             /* накапливаем целую цасть */
+    if (isdigit (c) || c == '-')                /* накапливаем целую цасть */
         while (isdigit(s[++i] = c = getch()));
 
-    if (c == '.')                /* накапливаем дробную часть */
+    if (c == '.')                               /* накапливаем дробную часть */
         while (isdigit(s[++i] = c = getch()));
 
     s[i] = '\0';
