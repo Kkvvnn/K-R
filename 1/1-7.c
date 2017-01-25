5c70b056e8a23daf1a22a77482236ac24fb15216
Kkvvnn <vova6281527@gmail.com>
2017-01-26 01:04:01 +0500

    глава 1 - добавил автора и email

--- 1/1-7.c
+++ 1/1-7.c
@@ -1,5 +1,8 @@
-/*      Упражнение 1.7.
- *  Напишите программу, печатающую значение EOF. */
+/*  Written by Vladimir Kashin <kkvvnnrussia@gmail.com>
+ *
+ *      Упражнение 1.7.
+ *  Напишите программу, печатающую значение EOF.
+ */
 
 #include <stdio.h>
 
